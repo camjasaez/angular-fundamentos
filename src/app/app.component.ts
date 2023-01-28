394c144854cd39d975e3fc6b5e5b282cbfa9d9c9
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-01-28 12:51:53 -0300

    Inicio con property binding e interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-market';
+
+  btnDisable = true;
+
+  person = {
+    name: 'Camilo',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
 }
