2646dfc16e3ab2b440c7c29e5676694af4199839
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-01-31 11:35:28 -0300

    NgIf NgFor parte3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IProduct} from "./product.model";
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,40 @@ export class AppComponent {
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+
+  products:IProduct[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/glasses.jpg'
+    }
+  ]
 
   name:string = ''
   names:string[] = [' Name1',' Name2', " Name3"," Name4"]
---
+++ src/app/product.model.ts
@@ -0,0 +1,6 @@
+export interface IProduct{
+  name:string;
+  price:number;
+  image:string;
+  category?:string;
+}
